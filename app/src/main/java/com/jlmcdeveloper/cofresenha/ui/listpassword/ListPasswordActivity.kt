f844ff750c2648458b0e505b654264c9ed304461
JulioMCDias <juliomarques11@gmail.com>
2020-06-03 11:34:53 -0300

    added view list password

---
+++ app/src/main/java/com/jlmcdeveloper/cofresenha/ui/listpassword/ListPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.jlmcdeveloper.cofresenha.ui.listpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jlmcdeveloper.cofresenha.R
+
+class ListPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_password)
+    }
+}
